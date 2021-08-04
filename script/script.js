8396cbbf352f41408cfc1e65a8e6afcc32ae35f8
Nicole Chang <powernicole2@yahoo.com.tw>
2021-08-04 22:14:58 +0800

    feat: tune UI

--- script/script.js
+++ script/script.js
@@ -50,7 +50,7 @@ app.component("card", {
   template: `<div class="col-md-6 py-2">
   <div class="card">
     <div class="card bg-dark text-white text-left">
-      <img class="card-img-top img-cover" height="155px" :src="item.Picture1">
+      <img class="card-img-top img-fluid" :src="item.Picture1">
       <div class="card-img-overlay d-flex justify-content-between align-items-end p-0 px-3" style="background-color: rgba(0, 0, 0, .2)">
         <h5 class="card-img-title-lg">{{ item.Name }}</h5><h5 class="card-img-title-sm">{{ item.Zone }}</h5>
       </div>
